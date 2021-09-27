035dd222dac54a39ec74b4f9b3633908f542d633
Gladston de Paula <gladston.dev@gmail.com>
2021-09-27 10:36:11 -0300

    Recentes deployed

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,7 +11,4 @@ function typeWriter(elemento) {
   const type1 = document.querySelector('.type1');
   typeWriter(type1);
 
-  const type2 = document.querySelector('.type2');
-  typeWriter(type2);
-
 /* Fim Animação Type Writer */
